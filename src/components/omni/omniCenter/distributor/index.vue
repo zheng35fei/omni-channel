82e4fd73dd2a12dc576797dd9d272ab19e1387e9
sendinfo_zhpf <sendinfo_zhpf@git.cloud.tencent.com>
2019-06-05 16:55:36 +0800

    修改接口地址

--- src/components/omni/omniCenter/distributor/index.vue
+++ src/components/omni/omniCenter/distributor/index.vue
@@ -163,7 +163,7 @@ export default {
             ],
             data: "",
             params: { page: 1, limit: 10, sort: "createTime", order: "desc" },
-            url: "/baseinfo/baseInfo/distributor/grid",
+            url: "/baseInfo/distributor/grid",
             content: "",
             mode: "",
             sucessMsg: ""
