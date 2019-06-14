7a0b14ca3b0b3f37054fd3bf23731338f06addc3
sendinfo_zhpf <sendinfo_zhpf@git.cloud.tencent.com>
2019-06-14 17:54:12 +0800

    权限接口

--- src/fetch/adminApi.js
+++ src/fetch/adminApi.js
@@ -34,7 +34,7 @@ export default {
     // 用户信息-启禁用设置
     setAccStatus: prefix + '/userInfo/setAccStatus',
     // 用户获取菜单列表树型
-    getMenuTreeData: perfix + '/sysRole/getSysMenuTree/',
-    getRolePlugs: perfix + '/sysRole/getSysRoleplugs/',
+    getMenuTreeData: prefix + '/sysRole/getSysMenuTree/',
+    getRolePlugs: prefix + '/sysRole/getSysRoleplugs/',
     setMenuToRole: prefix + '/sysRole/addSysMenuToRole/'
 }
