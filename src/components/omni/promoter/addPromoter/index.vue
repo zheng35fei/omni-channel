93f52cb4294a6b20bf4602efd25be38d1b62d986
sendinfo_zhpf <sendinfo_zhpf@git.cloud.tencent.com>
2019-08-07 16:00:18 +0800

    删除推广人身份证、营运证、导游证必填

--- src/components/omni/promoter/addPromoter/index.vue
+++ src/components/omni/promoter/addPromoter/index.vue
@@ -87,55 +87,6 @@ export default {
                 distId: [
                     { type: 'number', required: true, message: "请选择分销商", trigger: "change" }
                 ],
-                idCard: [
-                    { required: true, message: "请输入身份证号", trigger: "blur" },
-                    { validator: (rule, value, callback) => {
-                        // true "验证通过", false //校验不通过
-                        let flag = true;
-                        var format = /^(([1][1-5])|([2][1-3])|([3][1-7])|([4][1-6])|([5][0-4])|([6][1-5])|([7][1])|([8][1-2]))\d{4}(([1][9]\d{2})|([2]\d{3}))(([0][1-9])|([1][0-2]))(([0][1-9])|([1-2][0-9])|([3][0-1]))\d{3}[0-9xX]$/;
-                        //号码规则校验
-                        if(!format.test(value)){
-                            flag = false
-                        }
-                        //区位码校验
-                        //出生年月日校验   前正则限制起始年份为1900;
-                        var year = value.substr(6,4),//身份证年
-                            month = value.substr(10,2),//身份证月
-                            date = value.substr(12,2),//身份证日
-                            time = Date.parse(month+'-'+date+'-'+year),//身份证日期时间戳date
-                            now_time = Date.parse(new Date()),//当前时间戳
-                            dates = (new Date(year,month,0)).getDate();//身份证当月天数
-                        if(time>now_time||date>dates){
-                            flag = false
-                        }
-                        //校验码判断
-                        var c = new Array(7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2);   //系数
-                        var b = new Array('1','0','X','9','8','7','6','5','4','3','2');  //校验码对照表
-                        var id_array = value.split("");
-                        var sum = 0;
-                        for(var k=0;k<17;k++){
-                            sum+=parseInt(id_array[k])*parseInt(c[k]);
-                        }
-                        if(id_array[17] && id_array[17].toUpperCase() != b[sum%11].toUpperCase()){
-                            flag = false
-                        }
-                        if(!flag) {
-                            callback(new Error('身份证号格式不正确'))
-                        }else {
-                            callback()
-                        }
-
-                    }, trigger: 'blur'}
-                ],
-                idCardPicCode: [
-                    { required: true, message: "请上传身份证", trigger: "change" }
-                ],
-                touristCertPicCode: [
-                    { required: true, message: "请上传导游证", trigger: "change" }
-                ],
-                tradeCardPicCode: [
-                    { required: true, message: "请上传营运证", trigger: "change" }
-                ],
                 mobile: [
                     { required: true, message: "请输入手机号", trigger: "blur" },
                     { pattern: /^[1](([3][0-9])|([4][5-9])|([5][0-3,5-9])|([6][5,6])|([7][0-8])|([8][0-9])|([9][1,8,9]))[0-9]{8}$/, message: '请输入正确格式的手机号'}
